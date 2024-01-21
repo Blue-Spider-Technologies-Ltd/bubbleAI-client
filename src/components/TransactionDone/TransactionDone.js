0151ae81c1ce836ca6b44c808e323eac764d3196
papishef <sheriff.malebo@gmail.com>
2024-01-21 02:25:27 +0100

    TransactDone double fire fix

--- src/components/TransactionDone/TransactionDone.js
+++ src/components/TransactionDone/TransactionDone.js
@@ -1,6 +1,7 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef } from 'react';
 import { SuccessFailureModal } from '../UI/Modal/Modal';
 import { useLocation } from 'react-router-dom';
+import { Fetching } from '../UI/Modal/Modal';
 import axios from 'axios';
 
 
@@ -10,51 +11,56 @@ const TransactionDone = () => {
     const [isSuccessful, setIsSuccessful] = useState(false)
     const [isCompleted, setIsCompleted] = useState(false)
     const [transaction, setTransaction] = useState({})
+    const isEffectExecuted = useRef(false);
     
     useEffect(() => {
-        const completeTransaction = async () => {
-            try {
-                var params = new URLSearchParams(location.search);
-    
-                var status = params.get("status");
-                var txRef = params.get("tx_ref");
-                var transactionId = params.get("transaction_id");
-    
-              
-                const payload = {
-                    status : status,
-                    txRef: txRef,
-                    transactionId: transactionId
-                }
-    
-                const response = await axios.post("/pricing/complete-trasaction", payload, {
-                    headers: {
-                      "x-access-token": localStorage?.getItem('token'),
-                    },
-                });
-                console.log(response);
-                setIsCompleted(true)
-                
-                if(response?.data?.status === "successful") {
-                    setIsSuccessful(true)
-                    setTransaction(response.data)
-                } else {
-                    setTransaction(response.data)
+        if (!isEffectExecuted.current) {
+            const completeTransaction = async () => {
+                try {
+                    const params = new URLSearchParams(location.search);
+        
+                    const status = params.get("status");
+                    const txRef = params.get("tx_ref");
+                    const transactionId = params.get("transaction_id");
+        
+                  
+                    const payload = {
+                        status : status,
+                        txRef: txRef,
+                        transactionId: transactionId
+                    }
+        
+                    const response = await axios.post("/pricing/complete-trasaction", payload, {
+                        headers: {
+                          "x-access-token": localStorage?.getItem('token'),
+                        },
+                    });
+                    console.log(response);
                     setIsCompleted(true)
+                    
+                    if(response?.data?.status === "successful") {
+                        setTransaction(response.data)
+                        setIsSuccessful(true)
+                    } else {
+                        setTransaction(response.data)
+                        setIsCompleted(true)
+                    }
+                } catch (error) {
+                    setTransaction(error?.response?.data)
+                    setIsCompleted(true)
+                    console.log(error?.response?.data);
                 }
-            } catch (error) {
-                setTransaction(error?.response?.data)
-                setIsCompleted(true)
-                console.log(error?.response?.data);
             }
+            completeTransaction()
+            isEffectExecuted.current = true;
         }
-        completeTransaction()
+        
     }, [location])
 
 
     return (
         <div>
-            {isCompleted ? <SuccessFailureModal success={isSuccessful} fullName={transaction?.customer?.fullName} /> : ""}
+            {isCompleted ? <SuccessFailureModal success={isSuccessful} fullName={transaction?.customer?.fullName} /> : <Fetching />}
         </div>
     )
 }
