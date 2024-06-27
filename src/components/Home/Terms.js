949807422ae440ea099f0100cb771239d26c003e
papishef <sheriff.malebo@gmail.com>
2024-06-27 14:20:19 +0100

    refund policy update

--- src/components/Home/Terms.js
+++ src/components/Home/Terms.js
@@ -12,10 +12,10 @@ const Terms = () => {
       <MenuBar />
 
       <section id="not-found" className="container" style={{ marginTop: "70px" }}>
-        <h1 className="ask-me-h2" style={{textAlign: "center"}}>User Agreement and Privacy Policy</h1>
+        <h1 className="ask-me-h2" style={{textAlign: "center"}}>User Agreement, Privacy Policy & Refund Policy</h1>
 
         <p>This User Agreement and Privacy Policy <strong>("Agreement")</strong> is a legal agreement between you  
-        <strong>("User")</strong> and Blue Spider Technologies <strong>("Blue Spider")</strong> governing the use of Bubble AI 
+        <strong>("User")</strong> and Blue Spider Technologies Ltd <strong>("Blue Spider")</strong> governing the use of Bubble AI 
         software solution <strong>("Bubble AI")</strong>. By using Bubble AI, you agree to be bound by the terms and conditions set forth in this Agreement.</p>
 
         <section className="container-inner" style={{textAlign: "left", paddingBottom: "10px"}}>
@@ -43,6 +43,29 @@ const Terms = () => {
                 <p><strong>2.6 Updates:</strong> Blue Spider may update this Privacy Policy from time to time. User will be notified of any changes, and continued use of Bubble AI constitutes acceptance of the updated Privacy Policy.</p>
                 
             </div>
+        </section>
+
+
+        <section className="container-inner" style={{textAlign: "left", paddingBottom: "10px", marginTop: "20px"}}>
+            <h4 style={{color: "#56A8AC"}}>3. Refund Policy</h4>
+            <p>Thank you for using Bubble Ai services. We strive to provide our users with the best experience possible. In the event that you are not satisfied with our services, we have outlined our Refund Policy below:</p>
+
+            <div style={{paddingLeft: "20px", fontSize: ".9rem"}}>
+                <p><strong>3.1 Eligibility for Refund:</strong> <br />
+                  - Refunds will be considered on a case-by-case basis.<br />
+                  - Refunds may be provided if there are technical issues or errors on our end that prevent you from using our services.<br />
+                  - Refunds will not be provided for change of mind or if the services have been substantially delivered.</p>
+                <p><strong>3.2 Requesting a Refund:</strong> <br />
+                  - To request a refund, please contact our support team at <a className="link" href="support@bubble-ai.tech">support@bubble-ai.tech</a> with your reason for seeking a refund.<br />
+                  - Refund requests must be made within 5 days of the purchase or service delivery date.</p>
+                <p><strong>3.3 Processing of Refunds:</strong><br />
+                  - Once a refund request is received, we will assess the request and notify you of the approval or rejection of the refund with reason(s).
+                  - If approved, the refund will be processed within 7 business days and the amount will be credited back to the original payment method.</p>
+                <p><strong>3.4 Contact Us:</strong> <br />
+                  - If you have any questions about our Refund Policy or need further assistance, please contact our support team at <a className="link" href="support@bubble-ai.tech">support@bubble-ai.tech</a>.</p>
+            </div>
+
+            <p>Please note that this Refund Policy is subject to change without prior notice.</p>
         </section>
       </section>
 
